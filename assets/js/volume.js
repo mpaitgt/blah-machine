e1293cdc719220c2c2bc51b9467125b221e5f2cf
Matt Pignatore <mattpigs@gmail.com>
2020-04-15 11:45:16 -0400

    volume knob functionality implemeneted

--- assets/js/volume.js
+++ assets/js/volume.js
@@ -1,11 +1,14 @@
-let sound = document.querySelectorAll(`audio`);
 let volumeKnob = document.getElementById('volume-knob');
 
-console.log(sound);
-console.log(volumeKnob);
+// console.log(sound);
+// console.log(volumeKnob);
 
 volumeKnob.addEventListener('input', function(e) {
-  console.log('value', e.target.value / 100);
-  console.log('volume', sound.volume);
-  sound.volume = e.target.value / 100;
+  let sounds = document.querySelectorAll(`audio`);
+  sounds.forEach(sound => sound.volume = volumeKnob.value / 100);
+  
+  
+  
+  console.log('value', volumeKnob.value);
+  console.log('volume', sounds.volume);
 })
